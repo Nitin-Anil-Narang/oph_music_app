e95d5d24a13dbac7d2ef10c0972b259415174750
shreyash19-pixel <shreyashp734@gmail.com>
2025-07-09 13:00:53 +0530

    updated

--- backend/routes/audio_details.js
+++ backend/routes/audio_details.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const multer = require("multer");
-const { insertSongDetailsController } = require("../controllers/song_details");
+const { insertSongDetailsController } = require("../controllers/audio_details");
 
 const upload = multer({ storage: multer.memoryStorage() }); // For S3 upload
 
